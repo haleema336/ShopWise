30272c6ae497600f4d8736f26ff1a49c69596473
haleemasadiya <haleema.sadiya@lilly.com>
2024-09-19 20:45:40 +0530

    Modified

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from math import ceil
+from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
 db = SQLAlchemy(app)
 
+IST = pytz.timezone('Asia/Kolkata')
+
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     price = db.Column(db.Float)
-    category = db.Column(db.String(50)) 
-
+    category = db.Column(db.String(50))
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(IST))  # Corrected default
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     search_query = request.args.get('search', '')
-    sort_by = request.args.get('sort', 'name')
     category = request.args.get('category', '')
 
     page = request.args.get('page', 1, type=int)
@@ -30,10 +33,8 @@ def index():
     total_products = products_query.count()
     total_pages = ceil(total_products / per_page)
 
-    if sort_by == 'price':
-        products = products_query.order_by(Product.price).offset(offset).limit(per_page).all()
-    else:
-        products = products_query.order_by(Product.name).offset(offset).limit(per_page).all()
+    # Sort by created_at for the newest products first
+    products = products_query.order_by(Product.created_at.desc()).offset(offset).limit(per_page).all()
 
     return render_template('index.html', products=products, total_pages=total_pages, current_page=page)
 
@@ -43,14 +44,17 @@ def add_product():
     product_price = request.form['price']
     product_category = request.form['category']
     
-    new_product = Product(name=product_name, price=product_price, category=product_category)
+    new_product = Product(
+        name=product_name, 
+        price=product_price, 
+        category=product_category
+    )
     try:
         db.session.add(new_product)
         db.session.commit()
         return redirect('/')
     except:
         return 'There was an issue adding your product'
-
 
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
@@ -78,6 +82,5 @@ def delete(id):
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()
+        db.create_all()  # This will create the database and tables
     app.run(debug=True)
-
